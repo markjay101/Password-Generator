43bc48da96cc40c0866658588d8bc4a41aa3fb5e
markjay101 <markjayfuentescuyos1@gmail.com>
2023-03-01 22:59:10 +0800

    added some features

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var characters = []
-let generatePasswords = document.querySelector("#generate-password")
-let firstPassword = document.querySelector("#first-generated-pass")
-let secondPassword = document.querySelector("#second-generated-pass")
-let copyFirstPass = document.querySelector("#copy-first-pass-btn")
-let copySecondPass = document.querySelector("#copy-second-pass-btn")
+let characters = []
+var generatePasswords = document.querySelector("#generate-password")
+var firstPassword = document.querySelector("#first-generated-pass")
+var secondPassword = document.querySelector("#second-generated-pass")
+var copyFirstPass = document.querySelector("#copy-first-pass-btn")
+var copySecondPass = document.querySelector("#copy-second-pass-btn")
+var symbolsCheckbox = document.querySelector("#exclude-symbols")
+var numbersCheckbox = document.querySelector("#exclude-numbers")
+var passwordLength = document.querySelector("#pass-length")
 let textHint1 = false 
 let textHint2 = false
 let textHint3 = false
@@ -11,8 +14,43 @@ let interval = 0
 firstPassword.disabled = true
 secondPassword.disabled = true
 
-symbolsNumsLetters()
+//symbolsNumsLetters()
 renderHintText()
+
+function symbols(){
+    for(let i = 33; i < 48; i++){
+        let c = String.fromCharCode(i)
+        characters.push(c)
+    }
+    for(let i = 58; i < 65; i++){
+        let c = String.fromCharCode(i)
+        characters.push(c)
+    }
+    for(let i = 91; i < 97; i++){
+        let c = String.fromCharCode(i)
+        characters.push(c)
+    }
+    
+}
+
+function numbers(){
+    for(let i = 0; i < 10; i++)
+        characters.push(i)
+   
+}
+
+function letters(){
+    for(let i = 65; i < 91; i++){
+        let c = String.fromCharCode(i)
+        characters.push(c)
+    }
+    for(let i = 97; i < 123; i++){
+        let c = String.fromCharCode(i)
+        characters.push(c)
+    }
+   
+}
+
 function symbolsNumsLetters(){
     for(let i = 33; i < 127; i++){
         let c = String.fromCharCode(i)
@@ -26,8 +64,10 @@ function randomChars(){
 }
 
 function CompletePass(){
+    if(passwordLength.value < 8)
+        passwordLength.value = 8
     let completePassword = ""
-    for(let i = 0; i < 12; i++)
+    for(let i = 0; i < passwordLength.value; i++)
         completePassword += randomChars()
     return completePassword                   
 }
@@ -36,11 +76,30 @@ generatePasswords.addEventListener("click", function(){
     textHint1 = true
     textHint2 = false
     textHint3 = false
+    if(symbolsCheckbox.checked == true && numbersCheckbox.checked == true){
+        characters = []
+        letters()
+    }
+    else if(symbolsCheckbox.checked == true && numbersCheckbox.checked == false){
+        characters = []
+        numbers()
+        letters()
+    }
+    else if(symbolsCheckbox.checked == false && numbersCheckbox.checked == true){
+        characters = []
+        symbols()
+        letters()
+    }
+    else if(symbolsCheckbox.checked == false && numbersCheckbox.checked == false){
+        characters = []
+        symbolsNumsLetters()
+    }
     firstPassword.value = CompletePass()
     secondPassword.value = CompletePass()
     renderHintText()
     clearInterval(interval)
     interval = 0
+    console.log(characters)
 })
 
 copyFirstPass.addEventListener("click", function(){
@@ -116,4 +175,16 @@ function renderHintText(){
     }
 }
 
+symbolsCheckbox.addEventListener("click", function(){
+    if(symbolsCheckbox.checked == true)
+        document.querySelector("#checkbox-symbols").style.display = "none"
+    else
+        document.querySelector("#checkbox-symbols").style.display = "block"
+})
+numbersCheckbox.addEventListener("click", function(){
+    if(numbersCheckbox.checked == true)
+        document.querySelector("#checkbox-numbers").style.display = "none"
+    else
+        document.querySelector("#checkbox-numbers").style.display = "block"
+})
 
